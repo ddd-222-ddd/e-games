92b9418981b37fba948e085a75ae21c4abf99f93
Diego4w <diego444x@gmail.com>
2024-04-29 20:17:12 -0300

    estrutura de arquivos

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'))
+    res.sendFile(path.join(__dirname, './public', 'index.html'))
 });
 
 module.exports = router;
